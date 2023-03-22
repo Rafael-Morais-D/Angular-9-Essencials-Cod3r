5e4745e0bffabe5ce72bc64288b21eec45558fe3
RafaelMorais <rafaelmorais.design@gmail.com>
2023-03-22 15:29:52 -0300

    curso de Angular da Cod3r

---
+++ src/app/views/product-crud/product-crud.component.ts
@@ -0,0 +1,23 @@
+import { HeaderService } from './../../components/template/header/header.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-product-crud',
+  templateUrl: './product-crud.component.html',
+  styleUrls: ['./product-crud.component.css']
+})
+export class ProductCrudComponent {
+
+  constructor(private router: Router, headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
+  }
+
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create']);
+  }
+}
